4364907fb33924c2125ffeed13ed6290893a96a1
Deepak <kumarsainideepak32>
2024-07-09 07:52:26 +0530

    [Added] Server actions; Pagination; Error handling; server side form validation

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,9 +2,8 @@ import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
 import { Suspense } from 'react';
-import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '../../ui/skeletons';
  
 export default async function Page() {
   return (
